51a3c8a9214c5748ab7b0a28587c3f44acf1f5f3
s1F10250301 <s1F102503015@iniad.org>
2026-09-17 03:00:58 +0900

    lesson 01: add gain function

--- lessons/01-function-gain/starter/main.cpp
+++ lessons/01-function-gain/starter/main.cpp
@@ -1,13 +1,16 @@
 #include <iostream>
 
 // TODO: ApplyGain をここに書く
+float ApplyGain(float input, float gain) {
+    return input * gain;
+}
 
 int main() {
-    float input = 0.25f;
-    float gain = 2.0f;
+    float input = -0.25f;
+    float gain = 3.0f;
 
     // TODO: ApplyGainを呼び出してoutputに入れる
-    float output = 0.0f;
+    float output = ApplyGain(input, gain);
 
     std::cout << output << std::endl;
     return 0;

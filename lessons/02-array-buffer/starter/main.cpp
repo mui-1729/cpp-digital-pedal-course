2cd2c1c78b3a7460ca374fd3938b1ccef4c731b0
s1F10250301 <s1F102503015@iniad.org>
2026-09-17 03:13:45 +0900

    lesson 02: process audio buffer

--- lessons/02-array-buffer/starter/main.cpp
+++ lessons/02-array-buffer/starter/main.cpp
@@ -1,18 +1,25 @@
 #include <cstddef>
 #include <iostream>
 
-float ApplyGain(float input, float gain) {
+float ApplyGain(float input, float gain)
+{
     return input * gain;
 }
 
-int main() {
+int main()
+{
     constexpr std::size_t kSize = 8;
     float buffer[kSize] = {0.1f, 0.2f, 0.4f, -0.3f, -0.5f, 0.7f, -0.8f, 0.0f};
-    float gain = 2.0f;
+    float gain = 0.5f;
 
     // TODO: forでbufferの全要素にApplyGainを適用する
+    for (std::size_t i = 0; i < kSize; ++i)
+    {
+        buffer[i] = ApplyGain(buffer[i], gain);
+    }
 
-    for (std::size_t i = 0; i < kSize; ++i) {
+    for (std::size_t i = 0; i < kSize; ++i)
+    {
         std::cout << buffer[i] << '\n';
     }
 }

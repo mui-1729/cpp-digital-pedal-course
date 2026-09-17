d14d0669d302969a4459ed6c5528f6df45eeab76
s1F10250301 <s1F102503015@iniad.org>
2026-09-17 20:59:17 +0900

    lesson 03: process sample by reference

--- lessons/03-reference-in-place/starter/main.cpp
+++ lessons/03-reference-in-place/starter/main.cpp
@@ -1,12 +1,18 @@
 #include <iostream>
 
 // TODO: ApplyGainInPlace(float& sample, float gain)
+void ApplyGainInPlace(float &sample, float gain)
+{
+    sample *= gain;
+}
 
-int main() {
+int main()
+{
     float sample = 0.25f;
     std::cout << "before: " << sample << '\n';
 
     // TODO: gain=2.0fで呼ぶ
+    ApplyGainInPlace(sample, 2.0f);
 
     std::cout << "after : " << sample << '\n';
 }

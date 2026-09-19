b4fce4d69f3b2b04e73b8263c2d1c3d54c5011c9
s1F10250301 <s1F102503015@iniad.org>
2026-09-20 02:13:57 +0900

    lesson 04: process pointer buffer

--- lessons/04-pointer-buffer/starter/main.cpp
+++ lessons/04-pointer-buffer/starter/main.cpp
@@ -2,14 +2,23 @@
 #include <iostream>
 
 // TODO: ProcessBuffer を書く
+void ProcessBuffer(float *buffer, std::size_t size, float gain)
+{
+    for (std::size_t i = 0; i < size; ++i)
+    {
+        buffer[i] *= gain;
+    }
+}
 
-int main() {
+int main()
+{
     constexpr std::size_t kSize = 6;
     float buffer[kSize] = {0.1f, 0.2f, -0.3f, 0.4f, -0.5f, 0.6f};
 
-    // TODO: gain=2.0fでbuffer全体を処理する
+    ProcessBuffer(buffer, kSize, 2.0f);
 
-    for (std::size_t i = 0; i < kSize; ++i) {
+    for (std::size_t i = 0; i < kSize; ++i)
+    {
         std::cout << buffer[i] << '\n';
     }
 }

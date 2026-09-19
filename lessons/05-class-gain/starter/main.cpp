6fc73c8013d53af5b51e9522e05cf48279e02d7b
s1F10250301 <s1F102503015@iniad.org>
2026-09-20 02:26:13 +0900

    lesson 05: introduce gain class

--- lessons/05-class-gain/starter/main.cpp
+++ lessons/05-class-gain/starter/main.cpp
@@ -1,9 +1,19 @@
 #include <iostream>
 
 // TODO: class Gain を作る
+class Gain
+{
+public:
+    float Process(float input, float gain)
+    {
+        return input * gain;
+    };
+};
 
-int main() {
+int main()
+{
+    Gain gainEffect;
     // TODO: Gain objectを作り、Processを呼ぶ
-    float output = 0.0f;
+    float output = gainEffect.Process(0.25f, 2.0f);
     std::cout << output << '\n';
 }
